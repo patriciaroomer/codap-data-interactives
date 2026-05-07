8ff892c778772121e153726939c7af7d6c188b87
trish <ptrm1926@hochschule-trier.de>
2026-05-07 08:40:57 +0200

    language buttons for prompts

--- NLP/js/ui/PromptListener.js
+++ NLP/js/ui/PromptListener.js
@@ -7,21 +7,56 @@ export default class PromptListener extends UIListener {
   constructor() {
     super();
 
-    this.englishPrompts = document.getElementById("enPrompts").getElementsByTagName("td");
-    this.germanPrompts = document.getElementById("dePrompts").getElementsByTagName("td");
-    this.frenchPrompts = document.getElementById("frPrompts").getElementsByTagName("td");
+    this.tables = [document.getElementById("enPrompts"), document.getElementById("dePrompts"), document.getElementById("frPrompts")];
+
+    const englishPrompts = this.tables[0].getElementsByTagName("td");
+    const germanPrompts = this.tables[1].getElementsByTagName("td");
+    const frenchPrompts = this.tables[2].getElementsByTagName("td");
 
     let i = 0;
-    for (const prompt of this.englishPrompts) {
-      this.addListener(prompt);
-      this.addListener(this.germanPrompts[i]);
-      this.addListener(this.frenchPrompts[i]);
+    for (const prompt of englishPrompts) {
+      this.addPromptListener(prompt);
+      this.addPromptListener(germanPrompts[i]);
+      this.addPromptListener(frenchPrompts[i]);
+      i++;
     }
+
+    this.englishButton = document.getElementById("enButton");
+    this.germanButton = document.getElementById("deButton");
+    this.frenchButton = document.getElementById("frButton");
+
+    this.addEnglishListener();
+    this.addGermanListener();
+    this.addFrenchListener();
   }
 
-  addListener(prompt) {
+  addPromptListener(prompt) {
     prompt.addEventListener("click", () => {
       this.inputField.value = prompt.textContent;
+    });
+  }
+
+  addEnglishListener() {
+    this.englishButton.addEventListener("click", () => {
+      document.getElementById("dePrompts").classList.add("hidden");
+      document.getElementById("frPrompts").classList.add("hidden");
+      document.getElementById("enPrompts").classList.remove("hidden");
+    });
+  }
+
+  addGermanListener() {
+    this.germanButton.addEventListener("click", () => {
+      document.getElementById("enPrompts").classList.add("hidden");
+      document.getElementById("frPrompts").classList.add("hidden");
+      document.getElementById("dePrompts").classList.remove("hidden");
+    });
+  }
+
+  addFrenchListener() {
+    this.frenchButton.addEventListener("click", () => {
+      document.getElementById("enPrompts").classList.add("hidden");
+      document.getElementById("dePrompts").classList.add("hidden");
+      document.getElementById("frPrompts").classList.remove("hidden");
     });
   }
 }
